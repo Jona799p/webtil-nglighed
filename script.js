d36473b37ed41d45acc91a232ddfbc80e71c84f8
CyAim <jonaswedelgreen@gmail.com>
2024-10-28 14:11:10 +0100

    Nav (Not optimized)

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
 var x = document.getElementById("navigation");
-const elements = document.getElementsByClassName("navp");
-
+var y = document.getElementsByClassName("navli");
+var l = document.getElementById("img")
+var elements = document.getElementsByClassName("navp");
+var q = document.getElementById("Hamburger");
+var k = document.getElementById("cross")
 
 x.addEventListener("mouseover", open);
 x.addEventListener("mouseout", close);
+q.addEventListener("click", open)
+k.addEventListener("click", close)
+
 
 function open() {
     
-    x.style.animation="open 0.1s linear forwards";
+    l.style.display = "block";
+    x.style.animation = "open 0.1s linear forwards";
 
     for (var i = 0; i < elements.length; i++) {
         elements[i].style.display = "block"
     }
+
+    for (var n = 0; n < y.length; n++) {
+        y[n].style.justifyContent = "space-between"
+    }
+
 }
 
 function close() {
 
+    l.style.display = "none"
     x.style.animation="close 0.1s linear forwards";
 
     for (var i = 0; i < elements.length; i++) {
         elements[i].style.display = "none"  
     }
+
+    for (var n = 0; n < y.length; n++) {
+        y[n].style.justifyContent = "center"
+    }
+
 }
