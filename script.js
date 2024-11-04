a3748e73fb03b8b4d0ee2fbb184d1b8a0a8c1bde
CyAim <jonaswedelgreen@gmail.com>
2024-11-04 15:06:15 +0100

    Søge-felt 0.1

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const nav = document.querySelector("nav");
 const menu = document.getElementById("menu")
 const cross = document.getElementById("cross");
-
+const search = document.querySelector(".søg-felt");
+const searchField = document.querySelector(".søg-felt")
 
 const open = () => {
     nav.style.animation = "open 0.1s linear forwards"
@@ -11,8 +12,13 @@ const close = () => {
     nav.style.animation = "close 0.1s linear forwards"
 }
 
+const søg = () => {
+    search.style.border ="2px solid black"
+}
+
 
 nav.addEventListener("mouseover", open);
 nav.addEventListener("mouseout", close);
 menu.addEventListener("click", open);
 cross.addEventListener("click", close);
+searchField.addEventListener("active", søg);
